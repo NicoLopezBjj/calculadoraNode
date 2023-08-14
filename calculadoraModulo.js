ed6a72a9b068c20079090eb2ca4bb2fa45d0744b
Nico <lopeznnr@gmail.com>
2023-08-14 20:29:08 -0300

    mejore opciones modulo.js

--- calculadoraModulo.js
+++ calculadoraModulo.js
@@ -5,12 +5,27 @@ const multiplicar = require('./multiplicar');
 const dividir = require('./dividir');
 
 function calcular() {
+    
+    console.log("\n")
     console.log("Bienvenido a la calculadora NODE");
-    const opciones = ["Suma", "Resta", "Multiplicacion", "Dividir", "Salir"];
-    const index = readLine.keyInSelect(opciones, "Elegi la operacion que deseas realizar:");
+    console.log("1. Suma");
+    console.log("2. Resta");
+    console.log("3. Multiplicacion");
+    console.log("4. Dividir");
+    console.log("5. Salir")
 
-    if (index === 4) {
+    const opcion = readLine.questionInt("Elige la operacion que deseas realizar (1-4), o 5 para salir: ");
+
+    if (opcion === 5) {
         console.log("¡Hasta luego!");
+        return;
+    }
+
+    if (opcion < 1 || opcion > 5) {
+        console.log("\n")
+        console.log("Opción inválida.");
+        readLine.keyInPause("Presiona cualquier tecla para continuar...");
+        calcular();
         return;
     }
 
@@ -18,20 +33,20 @@ function calcular() {
     const segundoNumero = readLine.question("Escribi tu segundo numero: ");
     let result = 0
 
-    switch (index) {
-        case 0:
+    switch (opcion) {
+        case 1:
             result = sumar(parseInt(primerNumero), parseInt(segundoNumero));
             console.log(`El resultado de tu suma es ${result}`);
             break;
-        case 1:
+        case 2:
             result = restar(parseInt(primerNumero), parseInt(segundoNumero));
             console.log(`El resultado de tu resta es ${result}`);
             break;
-        case 2:
+        case 3:
             result = multiplicar(parseInt(primerNumero), parseInt(segundoNumero));
             console.log(`El resultado de tu multiplicacion es ${result}`);
             break;
-        case 3:
+        case 4:
             result = dividir(parseInt(primerNumero), parseInt(segundoNumero));
             console.log(`El resultado de tu division es ${result}`);
             break;
@@ -44,3 +59,4 @@ function calcular() {
 }
 
 calcular();
+
